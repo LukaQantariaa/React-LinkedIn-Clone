13451af209deb2f53add5b88ee5692cdb1ee9146
LukaQantariaa <l_kantaria@cu.edu.ge>
2021-09-07 23:30:01 +0400

    fix

--- src/Header.js
+++ src/Header.js
@@ -12,22 +12,26 @@ function Header() {
     return (
         <div className="header">
 
-            <div className="header__left">
-                <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt="" />
+            <div className="header__wrapper">
 
-                <div className="header__search">
-                    <SearchIcon />
-                    <input type="text" placeholder="Search" />
+                <div className="header__left">
+                    <img src="https://image.flaticon.com/icons/png/512/174/174857.png" alt="" />
+
+                    <div className="header__search">
+                        <SearchIcon />
+                        <input type="text" placeholder="Search" />
+                    </div>
                 </div>
-            </div>
 
-            <div className="header__right">
-                <HeaderOption title="Home" Icon={HomeIcon} />
-                <HeaderOption title="My Network" Icon={SupervisorAccount} />
-                <HeaderOption title="Jobs" Icon={BusinessCenter} />
-                <HeaderOption title="Messaging" Icon={Chat} />
-                <HeaderOption title="Notifications" Icon={Notifications} />
-                <HeaderOption avatar="https://images.unsplash.com/photo-1569173112611-52a7cd38bea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" title="me" />
+                <div className="header__right">
+                    <HeaderOption title="Home" Icon={HomeIcon} />
+                    <HeaderOption title="My Network" Icon={SupervisorAccount} />
+                    <HeaderOption title="Jobs" Icon={BusinessCenter} />
+                    <HeaderOption title="Messaging" Icon={Chat} />
+                    <HeaderOption title="Notifications" Icon={Notifications} />
+                    <HeaderOption avatar="https://images.unsplash.com/photo-1569173112611-52a7cd38bea9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80" title="me" />
+                </div>
+
             </div>
 
         </div>
